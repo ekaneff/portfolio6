a9cbdb54849f1806d09788b7d4989b55a7d3eebe
= <CaseyMcClure@Caseys-MacBook-Pro.local>
2017-01-07 12:06:10 -0500

    Created products migration

---
+++ teamwork/Laravel-ShoppingCart/database/migrations/2017_01_07_165012_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            //Define table columns here
+            $table->increments('id');
+            $table->string('name');
+            $table->mediumText('description');
+            $table->integer('price');
+            $table->integer('inStock');
+            $table->string('imgPath');
+
+            $table->index('name');
+            $table->index('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
